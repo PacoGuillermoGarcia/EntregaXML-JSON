a985a10851822bf678ccc51d16c0e1db440b0f15
pakotoes3@gmail.com <paco@Paco>
2019-03-25 12:26:13 +0100

    He realizado el menu y finalizado el ejercicio

--- EjerciciosJSON/EjerciciosJSON.py
+++ EjerciciosJSON/EjerciciosJSON.py
@@ -53,37 +53,47 @@ def tituloyposter(fecha1,fecha2,datos):
 import json
 with open ("movies.json") as fichero:
 	datos=json.load(fichero)
-#Ejercicio1
-	#for elem in tituloañoduracion(datos):
-#		print("Titulo:",elem[0])
-#		print("Año:",elem[1])
-#		print("Duracion:",elem[2])
-#		print("===================")
-
-#Ejercicio2
-	#for elem in contaractores(datos):
-	#	print("La pelicula %s tiene %i actores/actrices"%(elem[0],elem[1]))
-	#	print("================================================================================	")
-#Ejercicio3
-#	cad=input("Dime una palabras: ")
-#	cad2=input("Dime otra palabra: ")
-#	print("Las peliculas que tienen esas dos palabras en su sinopsis son: ")
-#	for elem in mostrarpeliculas(cad,cad2,datos):
-#		print(elem)
-#Ejercicio4
-#	actor=input("Dime un actor: ").title()
-#	print("Las peliculas del actor %s son: "%actor)
-#	for elem in peliculasdeactor(actor,datos):
-#		print(elem)
-
-#Ejercicio5
-	fecha1=input("Dime una fecha(año-mes-dia): ")
-	fecha2=input("Dime otra fecha(año-mes-dia): ")
-	while fecha1 >= fecha2:
-		print("La Segunda fecha debe ser mayor que la primera")
-		fecha1=input("Dime una fecha(año-mes-dia): ")
-		fecha2=input("Dime otra fecha(año-mes-dia): ")
-	for elem in tituloyposter(fecha1,fecha2,datos):
-		print("Titulo:",elem[0])
-		print("Posterurl:",elem[1])
-		print("======================================================")
+	while True:
+		print('''
+			1.-Listar titulo, año y duración de todas las películas
+			2.-Mostrar titulos y la cantidad de actores
+			3.-Dar 2 palabras por teclado y buscar las películas que contengan en la sinopsis esas dos palabras
+			4.-Introducir un actor y mostrar las películas en las que haya trabajado
+			5.-Introducir dos fechas y obtener el título y la url del póster de las películas con una media mas alta lanzada entre esas dos fechas
+			0.-Salir''')
+		opcion=input("Opcion: ")
+		if opcion=="1":
+			for elem in tituloañoduracion(datos):
+				print("Titulo:",elem[0])
+				print("Año:",elem[1])
+				print("Duracion:",elem[2])
+				print("===================")
+		elif opcion=="2":
+			for elem in contaractores(datos):
+				print("La pelicula %s tiene %i actores/actrices"%(elem[0],elem[1]))
+			print("================================================================================	")
+		elif opcion=="3":
+			cad=input("Dime una palabras: ")
+			cad2=input("Dime otra palabra: ")
+			print("Las peliculas que tienen esas dos palabras en su sinopsis son: ")
+			for elem in mostrarpeliculas(cad,cad2,datos):
+				print(elem)
+		elif opcion=="4":
+			actor=input("Dime un actor: ").title()
+			print("Las peliculas del actor %s son: "%actor)
+			for elem in peliculasdeactor(actor,datos):
+				print(elem)
+		elif opcion=="5":
+			fecha1=input("Dime una fecha(año-mes-dia): ")
+			fecha2=input("Dime otra fecha(año-mes-dia): ")
+			while fecha1 >= fecha2:
+				print("La Segunda fecha debe ser mayor que la primera")
+				fecha1=input("Dime una fecha(año-mes-dia): ")
+				fecha2=input("Dime otra fecha(año-mes-dia): ")
+			for elem in tituloyposter(fecha1,fecha2,datos):
+				print("Titulo:",elem[0])
+				print("Posterurl:",elem[1])
+				print("======================================================")
+		elif opcion=="0":
+			print("Adios")
+			break
